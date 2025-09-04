0ced16d70daab4a5e77735aab0c372268f34f293
GK Prasad <ganeshkadoorprasad@gmail.com>
2025-09-04 15:31:23 +0530

    added sentiment analysis

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_template
 
-app=Flask(__name__)
+from flask import Flask, render_template, request
+from transformers import pipeline
 
-@app.route("/")
+# Load Hugging Face pipeline (sentiment analysis)
+classifier = pipeline("sentiment-analysis")
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html", name="Prasad")
+    result = None
+    if request.method == "POST":
+        text = request.form["user_input"]
+        result = classifier(text)[0]  # Example: {'label': 'POSITIVE', 'score': 0.99}
+    return render_template("index.html", result=result)
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=5000,debug=True)
